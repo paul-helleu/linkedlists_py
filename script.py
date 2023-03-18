d92d3e3daaafd837c1e088d527209279ba9ee922
PauL <57185255+paulh-afk@users.noreply.github.com>
2023-03-18 19:18:53 +0100

    node variables

--- script.py
+++ script.py
@@ -11,3 +11,16 @@ class Node:
   
   def get_value(self):
     return self.value
+
+yacko = Node('likes to yak')
+wacko = Node('has a penchant for hoarding snacks')
+dot = Node('enjoys spending time in movie lots')
+
+yacko.set_link_node(dot)
+dot.set_link_node(wacko)
+
+dots_data = yacko.get_link_node().get_value()
+wackos_data = dot.get_link_node().get_value()
+
+print(dots_data)
+print(wackos_data)
