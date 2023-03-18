c13f461c6fe8e0fbf94e438cd6ff7ad6d85f1acf
PauL <57185255+paulh-afk@users.noreply.github.com>
2023-03-18 23:59:39 +0100

    init LinkedList class

--- script.py
+++ script.py
@@ -11,16 +11,12 @@ class Node:
   
   def get_value(self):
     return self.value
+  
 
-yacko = Node('likes to yak')
-wacko = Node('has a penchant for hoarding snacks')
-dot = Node('enjoys spending time in movie lots')
-
-yacko.set_link_node(dot)
-dot.set_link_node(wacko)
-
-dots_data = yacko.get_link_node().get_value()
-wackos_data = dot.get_link_node().get_value()
-
-print(dots_data)
-print(wackos_data)
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+  
