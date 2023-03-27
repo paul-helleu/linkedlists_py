547cc9c43dd190e3b6613be35f5a5f3b7e7f9430
PauL <57185255+paulh-afk@users.noreply.github.com>
2023-03-27 19:19:54 +0200

    LinkedList methods
    LinkedList: get_head_node, insert_beginning methods.
    Get the head Node and insert a Node at the beginning.

--- script.py
+++ script.py
@@ -16,3 +16,10 @@ class Node:
 class LinkedList:
     def __init__(self, value=None):
         self.head_node = Node(value)
+
+    def get_head_node(self):
+        return self.head_node
+
+    def insert_beginning(self, new_value):
+        new_node = Node(new_value, self.head_node)
+        self.head_node = new_node
