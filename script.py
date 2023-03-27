b489b574417729f04abb08fbfa89668aeb593f3b
PauL <57185255+paulh-afk@users.noreply.github.com>
2023-03-27 19:21:10 +0200

    LinkedList swap_nodes_values

--- script.py
+++ script.py
@@ -71,3 +71,49 @@ class LinkedList:
                 current_node.set_next_node(next_node.get_next_node())
 
             current_node = current_node.get_next_node()
+
+    def swap_nodes_values(self, value1, value2):
+        # No change if value not found
+        if value1 == value2:
+            return
+
+        node1 = None
+        node2 = None
+
+        current_node = self.head_node
+
+        if current_node.get_value() == value1:
+            node1 = current_node
+        else:
+            while True:
+                next_node = current_node.get_next_node()
+                if next_node is None:
+                    break
+
+                if next_node.get_value() == value1:
+                    node1 = next_node
+                    break
+
+                current_node = next_node
+
+        current_node = self.head_node
+
+        if current_node.get_value() == value2:
+            node2 = current_node
+        else:
+            while True:
+                next_node = current_node.get_next_node()
+                if next_node is None:
+                    break
+
+                if next_node.get_value() == value2:
+                    node2 = next_node
+                    break
+
+                current_node = next_node
+
+        if node1 is not None and node2 is not None:
+            node1_value = node1.get_value()
+
+            node1.value = node2.get_value()
+            node2.value = node1_value
