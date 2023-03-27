2c4cd24143440cb139d5b582f1a79ee2837567b3
PauL <57185255+paulh-afk@users.noreply.github.com>
2023-03-27 19:20:50 +0200

    LinkedList remove methods

--- script.py
+++ script.py
@@ -35,3 +35,39 @@ class LinkedList:
             current_node = next_node
 
         return string_list
+
+    def remove_node(self, value_to_remove):
+        current_node = self.head_node
+
+        if current_node.get_value() == value_to_remove:
+            self.head_node = current_node.get_next_node()
+        else:
+            while current_node is not None:
+                next_node = current_node.get_next_node()
+
+                if next_node is None:
+                    break
+
+                if next_node.get_value() == value_to_remove:
+                    current_node.set_next_node(next_node.get_next_node())
+                    break
+
+                next_node = current_node.get_next_node()
+                current_node = next_node
+
+    def remove_nodes(self, value_to_remove):
+        current_node = self.head_node
+
+        while current_node is not None:
+            if current_node == self.head_node and current_node.get_value() == value_to_remove:
+                self.head_node = self.head_node.get_next_node()
+                continue
+
+            next_node = current_node.get_next_node()
+            if next_node == None:
+                break
+
+            if next_node.get_value() == value_to_remove:
+                current_node.set_next_node(next_node.get_next_node())
+
+            current_node = current_node.get_next_node()
